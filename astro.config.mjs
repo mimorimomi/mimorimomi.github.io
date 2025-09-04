e6b86aab2fe4b0dbcb68be0e3b2c52055ef9ade5
Mimori Fujino <41240321+mimorimomi@users.noreply.github.com>
2025-09-04 20:39:59 +0900

    chore: site 設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mimorimomi.github.io",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
